e6a6c0fbe5ee9f0efb10773660c098fc6959d391
seojoo <seojoo21@naver.com>
2023-05-17 22:41:45 +0900

    20230517: Reverse LinkedList by Iterative Method

--- Java/LinkedList/LinkedList.java
+++ Java/LinkedList/LinkedList.java
@@ -18,11 +18,32 @@ public class LinkedList {
         }
     }
 
+    public static ListNode iterativeReverseList(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+
+        return head;
+    }
+
     public static void main(String[] args){
         ListNode lnode3 = new ListNode(3);
         ListNode lnode2 = new ListNode(2, lnode3);
         ListNode lnode1 = new ListNode(1, lnode2);
 
+        System.out.println("1. print out linked list");
+        printList(lnode1);
+
+        System.out.println("\n2. reverse linked list");
+        lnode1 = iterativeReverseList(lnode1);
         printList(lnode1);
     }
 }
