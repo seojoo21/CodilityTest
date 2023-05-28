d29dccbd1019494e69dfffc1f1444f4131f8c5dc
seojoo <seojoo21@naver.com>
2023-05-28 21:00:58 +0900

    20230528: Graph DFS problem - Keys and Rooms.

---
+++ Java/Graph/DFS/KeysAndRooms.java
@@ -0,0 +1,27 @@
+package Graph.DFS;
+
+import java.util.*;
+
+// https://leetcode.com/problems/keys-and-rooms/description
+public class KeysAndRooms {
+    public static boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited.add(0);
+        queue.offer(0);
+
+        while(!queue.isEmpty()){
+            int rn = queue.poll();
+            List<Integer> keys = rooms.get(rn);
+            for(int key : keys){
+                if(!visited.contains(key)){
+                    visited.add(key);
+                    queue.offer(key);
+                }
+            }
+        }
+        if(visited.size() == rooms.size()) return true;
+        return false;
+    }
+}
